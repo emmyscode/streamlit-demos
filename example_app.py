f19804a97931c07102cd41ff8694829dee1eb760
Emmy Li <78717839+emmyscode@users.noreply.github.com>
2022-07-21 10:32:05 -0700

    Update example_app.py

--- example_app.py
+++ example_app.py
@@ -4,5 +4,21 @@ import numpy as np
 import pandas as pd
 import numpy as np
 
-st.title("Sketch Recognition")
-st.header("Introduction to Computer Vision")
+st.set_page_config(layout='wide')
+
+st.sidebar.title("About")
+st.sidebar.info(
+    """
+    Web App URL: <https://emmyscode-streamlit-example-app-ojc2pw.streamlitapp.com>
+    \n
+    GitHub Respository: <https://github.com/emmyscode/streamlit>
+    """
+)
+
+st.title("Example App")
+st.header("A Machine Learning Web Application Deployed by Streamlit")
+st.markdown(
+    """
+    Welcome to this example app created using [streamlit](https://streamlit.io)!
+    """
+)
